87ee2c7b818ac3ba02b9122f247ff6a98c3a1860
Adrian <palebluedot85@gmail.com>
2022-02-14 14:46:40 -0500

    fixed the calculation logic

--- script.js
+++ script.js
@@ -5,27 +5,25 @@ const taxTotal = document.querySelector("[data-tax-total]");
 const unusedFare = document.querySelector("[data-unused-fare]");
 const surcharge = document.querySelector("[data-surcharge]");
 const penalty = document.querySelector("[data-penalty]");
-const nuc = document.querySelector("#nuc-calc");
-const nucCheckBox = document.querySelector("#nuc");
-const nucBlock = document.querySelector(".nuc-calc-block");
-const totalAmount = document.querySelector("[data-total-receivable]");
+const nuc = document.querySelector("#nuc-calc")
+const nucCheckBox = document.querySelector("#nuc")
+const nucBlock = document.querySelector(".nuc-calc-block")
+const totalAmount = document.querySelector("[data-total-receivable]")
 
 let data = [];
 let obj = {};
-let totalTaxAmount = 0;
+let sumTaxes = 0;
 let farePortion = 0;
 let qSurcharge = 0;
 let penalties = 0;
-let nucValue = 0;
-let totalBaseFare = 0;
-let totBaseWithNuc = 0;
-let grandTotalAmount = 0;
+let nucValue = 0
+let totalBaseFare = 0
+let totBaseWithNuc = 0
+let grandTotal = 0
 
 nucCheckBox.addEventListener("click", (e) => {
-  e.target.checked
-    ? (nucBlock.style.display = "block")
-    : (nucBlock.style.display = "none");
-});
+    e.target.checked ? nucBlock.style.display = "block" : nucBlock.style.display = "none"
+})
 
 taxBox1.forEach((box) => {
   box.addEventListener("change", (e) => {
@@ -49,64 +47,73 @@ taxCode.forEach((code) => {
 
 unusedFare.addEventListener("change", (e) => {
   farePortion = e.target.value;
-  totalBase();
+  totalBase()
+  sumAllNumbers()
 });
 
 surcharge.addEventListener("change", (e) => {
   qSurcharge = e.target.value;
-  totalBase();
+  totalBase()
+  sumAllNumbers()
 });
 
-nuc.addEventListener("change", (e) => {
-  nucValue = e.target.value;
-  calculateNuc();
-});
+nuc.addEventListener('change', (e) => {
+    nucValue = e.target.value
+    calculateNuc()
+    sumAllNumbers()
+})
 
 penalty.addEventListener("change", (e) => {
-  penalties = e.target.value;
-  addPenalty();
-});
+    penalties = e.target.value;
+    sumAllNumbers()
+  });
 
 function calculate(taxCode) {
-  let num = 0;
   if (data[0] && data[1]) {
-    if (parseFloat(data[1]) > parseFloat(data[0])) {
-      num = parseFloat(data[1]) - parseFloat(data[0]);
-      num = num.toFixed(2);
+    if (parseFloat(data[0]) > parseFloat(data[1])) {
+      tax = parseFloat(data[0]) - parseFloat(data[1]);
+      tax = tax.toFixed(2);
     } else {
-      num = 0;
+      tax = 0;
     }
   }
-  obj[taxCode] = num;
+  obj[taxCode] = tax;
   totalTax();
 }
 
 function totalTax() {
+  sumTaxes = 0
   for (let total in obj) {
     if (obj !== undefined) {
-      totalTaxAmount += parseFloat(obj[total]);
+      sumTaxes += parseFloat(obj[total]);
     }
   }
-//   totalTaxAmount = totalTaxAmount.toFixed(2);
-  taxTotal.innerText = totalTaxAmount;
+  taxTotal.innerText = sumTaxes;
 }
 
 function totalBase() {
-  totalBaseFare = parseFloat(farePortion) + parseFloat(qSurcharge);
-  totalBaseFare = totalBaseFare.toFixed(2);
+    totalBaseFare = parseFloat(farePortion) + parseFloat(qSurcharge)
+    totalBaseFare = totalBaseFare.toFixed(2)
 }
 
 function calculateNuc() {
-  totBaseWithNuc = parseFloat(totalBaseFare) * parseFloat(nucValue);
-  console.log(totBaseWithNuc);
+    totBaseWithNuc = parseFloat(totalBaseFare) * parseFloat(nucValue)
+    console.log(totBaseWithNuc)
 }
 
-function addPenalty() {
-  if (totBaseWithNuc > 0) {
-    console.log(parseFloat(penalties) + parseFloat(totBaseWithNuc));
-  } else {
-    console.log(parseFloat(penalties) + parseFloat(totalBaseFare));
-  }
+// function addPenalty() {
+//     if(totBaseWithNuc > 0) {
+//         console.log(penalties + totBaseWithNuc)
+//     } else {
+//         console.log(penalties + totalBaseFare)
+//     }
+// }
+
+function sumAllNumbers() {
+    if(nucValue) {
+      grandTotal = parseFloat(totBaseWithNuc) + parseFloat(sumTaxes) - parseFloat(penalties)
+    } else {
+      grandTotal = parseFloat(totalBaseFare) + parseFloat(sumTaxes) - parseFloat(penalties)
+    }
+    totalAmount.innerText = grandTotal
 }
-
-
