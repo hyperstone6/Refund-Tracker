8bdf6c1f5cd3ece118774e24669537ace1977e23
Adrian <palebluedot85@gmail.com>
2022-02-26 20:10:36 -0500

    added tax parser function

--- calc_logic.js
+++ calc_logic.js
@@ -12,7 +12,7 @@ const totalAmount = document.querySelector("[data-total-receivable]");
 
 let obj = {};
 let sumTaxes = 0;
-let sumAllTaxes = 0
+let sumAllTaxes = 0;
 let farePortion = 0;
 let qSurcharge = 0;
 let penalties = 0;
@@ -23,67 +23,84 @@ let grandTotal = 0;
 
 let taxObj = {};
 
+let newObj = {};
+
 taxBox1.forEach((box) => {
-  box.addEventListener("input", (e) => {
-    let prev = e.target.previousElementSibling
-    let next = e.target.nextElementSibling.nextElementSibling
-    if (taxObj.hasOwnProperty(prev.value)) {
-      codeInTaxBox = box.dataset.taxBox1
-      let taxCodeValue = prev.value
-      taxObj[taxCodeValue][codeInTaxBox] = parseFloat(e.target.value);
-      calculateTax(prev.value)
-      next.innerText = obj[prev.value].toFixed(2)
-      sumAllNumbers()
-    }
-  });
+  box.addEventListener("input", taxBox1Fn);
+  box.addEventListener("change", taxBox1Fn);
 });
 
 taxBox2.forEach((box) => {
-  box.addEventListener("input", (e) => {
-    let prev = e.target.previousElementSibling.previousElementSibling
-    let next = e.target.nextElementSibling
-    if (taxObj.hasOwnProperty(prev.value)) {
-    codeInTaxBox = box.dataset.taxBox2
-    let taxCodeValue = prev.value
-    taxObj[taxCodeValue][codeInTaxBox] = parseFloat(e.target.value);
-    calculateTax(prev.value)
-    next.innerText = obj[prev.value].toFixed(2)
-    sumAllNumbers()
-  }
-  });
+  box.addEventListener("input", taxBox2Fn);
+  box.addEventListener("change", taxBox2Fn);
 });
 
 taxCode.forEach((codeBox) => {
-  codeBox.addEventListener("input", (e) => {
-    e.target.value = e.target.value.toLocaleUpperCase()
-    if (e.target.value.length === 2 || e.target.value.length === 4) {
-      e.target.nextElementSibling.disabled = false
-      e.target.nextElementSibling.nextElementSibling.disabled = false
-      taxObj[e.target.value] = {};
-    } else {
-      e.target.nextElementSibling.disabled = true
-      e.target.nextElementSibling.nextElementSibling.disabled = true
-    }
+  codeBox.addEventListener("input", taxCodeFn);
+  codeBox.addEventListener("change", taxCodeFn);
+});
+
+function taxBox1Fn(e) {
+  let prev = e.target.previousElementSibling;
+  let next = e.target.nextElementSibling.nextElementSibling;
+  if (taxObj.hasOwnProperty(prev.value)) {
+    codeInTaxBox = e.target.dataset.taxBox1;
+    let taxCodeValue = prev.value;
+    taxObj[taxCodeValue][codeInTaxBox] = parseFloat(e.target.value);
+    calculateTax(prev.value);
+    next.innerText = obj[prev.value].toFixed(2);
+    sumAllNumbers();
+  }
+}
+
+function taxBox2Fn(e) {
+  let prev = e.target.previousElementSibling.previousElementSibling;
+  let next = e.target.nextElementSibling;
+  if (taxObj.hasOwnProperty(prev.value)) {
+    codeInTaxBox = e.target.dataset.taxBox2;
+    let taxCodeValue = prev.value;
+    taxObj[taxCodeValue][codeInTaxBox] = parseFloat(e.target.value);
+    calculateTax(prev.value);
+    next.innerText = obj[prev.value].toFixed(2);
+    sumAllNumbers();
+  }
+}
+
+function taxCodeFn(e) {
+  e.target.value = e.target.value.toLocaleUpperCase();
+  if (e.target.value.length === 2 || e.target.value.length === 4) {
+    e.target.nextElementSibling.disabled = false;
+    e.target.nextElementSibling.nextElementSibling.disabled = false;
+    taxObj[e.target.value] = {};
+  } else {
+    e.target.nextElementSibling.disabled = true;
+    e.target.nextElementSibling.nextElementSibling.disabled = true;
+  }
+}
+
+if (unusedFare !== null || unusedFare !== undefined) {
+  unusedFare.addEventListener("input", (e) => {
+    farePortion = e.target.value;
+    totalBase();
+    sumAllNumbers();
   });
-});
+}
 
-unusedFare.addEventListener("input", (e) => {
-  farePortion = e.target.value;
-  totalBase();
-  sumAllNumbers();
-});
+if (surcharge !== null || surcharge !== undefined) {
+  surcharge.addEventListener("input", (e) => {
+    qSurcharge = e.target.value;
+    totalBase();
+    sumAllNumbers();
+  });
+}
 
-surcharge.addEventListener("input", (e) => {
-  qSurcharge = e.target.value;
-  totalBase();
-  sumAllNumbers();
-});
-
-nuc.addEventListener("input", (e) => {
-  nucValue = e.target.value;
-  calculateNuc();
-  sumAllNumbers();
-});
+if (nuc !== null || nuc !== undefined) {
+  nuc.addEventListener("input", (e) => {
+    nucValue = e.target.value;
+    calculateNuc();
+    sumAllNumbers();
+  });
+}
 
 nucCheckBox.addEventListener("click", (e) => {
   e.target.checked
@@ -91,47 +108,51 @@ nucCheckBox.addEventListener("click", (e) => {
     : (nucBlock.style.display = "none");
 });
 
-penalty.addEventListener("input", (e) => {
-  penalties = e.target.value;
-  sumAllNumbers();
-});
+if (penalty !== null || penalty !== undefined) {
+  penalty.addEventListener("input", (e) => {
+    penalties = e.target.value;
+    sumAllNumbers();
+  });
+}
 
 function calculateTax(taxCodeToCalc) {
-  let oldTaxValue = 0
-  let newTaxValue = 0
-  let taxCodeObject = Object.keys(taxObj[taxCodeToCalc])
-  if(taxCodeObject[0]) {
-    if(taxCodeObject[0][2] == 'o') {
-      oldTaxValue = Object.values(taxObj[taxCodeToCalc])[0]
+  let oldTaxValue = 0;
+  let newTaxValue = 0;
+  let taxCodeObject = Object.keys(taxObj[taxCodeToCalc]);
+  if (taxCodeObject[0]) {
+    if (taxCodeObject[0][2] == "o") {
+      oldTaxValue = Object.values(taxObj[taxCodeToCalc])[0];
     } else {
-      oldTaxValue = Object.values(taxObj[taxCodeToCalc])[1]
+      oldTaxValue = Object.values(taxObj[taxCodeToCalc])[1];
     }
   }
-  if(taxCodeObject[1]) {
-    if(taxCodeObject[1][2] == 'n') {
-      newTaxValue = Object.values(taxObj[taxCodeToCalc])[1]
+  if (taxCodeObject[1]) {
+    if (taxCodeObject[1][2] == "n") {
+      newTaxValue = Object.values(taxObj[taxCodeToCalc])[1];
     } else {
-      newTaxValue = Object.values(taxObj[taxCodeToCalc])[0]
+      newTaxValue = Object.values(taxObj[taxCodeToCalc])[0];
     }
   }
-  if(oldTaxValue > newTaxValue) {
-    sumTaxes = oldTaxValue - newTaxValue
-  } else  {
-    sumTaxes = 0
+  if (oldTaxValue > newTaxValue) {
+    sumTaxes = oldTaxValue - newTaxValue;
+  } else if (oldTaxValue === Number(0)) {
+    sumTaxes = newTaxValue * -1;
+  } else {
+    sumTaxes = 0;
   }
-  obj[taxCodeToCalc] = sumTaxes
-  totalTax()
+  obj[taxCodeToCalc] = sumTaxes;
+  totalTax();
 }
 
 function totalTax() {
-  let totalOfAllTaxes = 0
+  let totalOfAllTaxes = 0;
   for (let total in obj) {
     if (obj !== undefined) {
       totalOfAllTaxes += parseFloat(obj[total]);
     }
   }
   taxTotal.innerText = totalOfAllTaxes.toFixed(2);
-  sumAllTaxes = totalOfAllTaxes
+  sumAllTaxes = totalOfAllTaxes;
 }
 
 function totalBase() {
@@ -147,10 +168,94 @@ function calculateNuc() {
 function sumAllNumbers() {
   if (nucValue) {
     grandTotal =
-      parseFloat(totBaseWithNuc) + parseFloat(sumAllTaxes) - parseFloat(penalties);
+      parseFloat(totBaseWithNuc) +
+      parseFloat(sumAllTaxes) -
+      parseFloat(penalties);
   } else {
     grandTotal =
-      parseFloat(totalBaseFare) + parseFloat(sumAllTaxes) - parseFloat(penalties);
+      parseFloat(totalBaseFare) +
+      parseFloat(sumAllTaxes) -
+      parseFloat(penalties);
   }
   totalAmount.innerText = grandTotal.toFixed(2);
+}
+//////////////////////////////////////////////////////////////////
+
+const scratchBtn = document.querySelectorAll("[data-parse-btn]");
+const scratchpad = document.querySelector("[data-scratchpad]")
+
+let pastedText = "";
+let foundTaxes = "";
+let splitted = [];
+let clndUp = [];
+let tot = 0;
+let regex;
+let testArr = [];
+
+scratchpad.addEventListener('input', (e)=> {
+    if(e.target.value.includes('CAD')) {
+      regex = /TX...CAD/g
+    } else if(e.target.value.includes('USD')) {
+      regex = /TX...USD/g
+    }
+})
+
+if (scratchBtn !== null && scratchBtn !== undefined) {
+  scratchBtn.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      
+      pastedText = e.target.previousElementSibling.value;
+      splitted = pastedText.split(regex);
+      for (let index of splitted) {
+        if (index) {
+          clndUp.push(index.replace(/\s/g, ""));
+        }
+      }
+      for(let i = 0; i < clndUp.length; i++) {
+        clndUp[i] = clndUp[i] + taxBox1[i].dataset.taxBox1.slice(0,2)
+      }
+      for (let index = 0; index < clndUp.length; index++) {
+        let clndKeys = clndUp[index];
+        if (!isNaN(parseFloat(clndKeys))) {
+          if (newObj[clndKeys.slice(clndKeys.length - 4)]) {
+            newObj[clndKeys.slice(clndKeys.length - 4)] +=
+              parseFloat(clndKeys);
+            alert(
+              `Taxes with same tax code were combined. Combined tax: ${clndKeys.slice(
+                clndKeys.length - 2
+              )}`
+            );
+          } else {
+            newObj[clndKeys.slice(clndKeys.length - 4)] =
+              parseFloat(clndKeys);
+            testArr.push({
+              [clndKeys.slice(clndKeys.length - 4)]:
+                parseFloat(clndKeys),
+            });
+          }
+        }
+      }
+      for (let i = 0; i < testArr.length; i++) {
+        let prev = taxBox1[i].previousElementSibling;
+        let next = taxBox1[i].nextElementSibling.nextElementSibling;
+        codeInTaxBox1 = taxBox1[i].dataset.taxBox1;
+        taxCode[i].value = Object.keys(testArr[i]);
+        taxCode[i].nextElementSibling.disabled = false;
+        taxCode[i].nextElementSibling.nextElementSibling.disabled = false;
+        taxObj[taxCode[i].value] = {};
+        taxBox1[i].value = Object.values(testArr[i]);
+        taxObj[prev.value][codeInTaxBox1] = parseFloat(taxBox1[i].value);
+        taxBox2[i].value = 0
+        
+        calculateTax(prev.value)
+        
+        next.innerText = obj[prev.value];
+      }
+      for (let num in newObj) {
+        if (newObj) {
+          tot += newObj[num];
+        }
+      }
+    });
+  });
 }
