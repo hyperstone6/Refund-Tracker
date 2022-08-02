3f5de4e33152697b2eb99430a5d2641bc3732185
Amon Ra II <palebluedot85@gmil.com>
2022-08-02 10:38:20 -0400

    added YouTube instructions link

--- calc_logic.js
+++ calc_logic.js
@@ -8,7 +8,7 @@ const penalty = document.querySelector("[data-penalty]");
 const nuc = document.querySelector("#nuc-calc");
 const nucCheckBox = document.querySelector("#nuc");
 const nucBlock = document.querySelector(".nuc-calc-block");
-const totalBaseOutput = document.querySelector("[data-fare-total]");
+const totalBaseOutput = document.querySelector('[data-fare-total]')
 const totalAmount = document.querySelector("[data-total-receivable]");
 
 let obj = {};
@@ -54,8 +54,8 @@ function taxBox2Fn(e) {
   let prev = e.target.previousElementSibling.previousElementSibling;
   let next = e.target.nextElementSibling;
   let boxValue = e.target.value;
-  if (boxValue === "") {
-    boxValue = 0;
+  if(boxValue === "") {
+    boxValue = 0
   }
   if (taxObj.hasOwnProperty(prev.value)) {
     codeInTaxBox = e.target.dataset.taxBox2;
@@ -82,8 +82,8 @@ function taxCodeFn(e) {
 if (unusedFare !== null || unusedFare !== undefined) {
   unusedFare.addEventListener("input", (e) => {
     farePortion = e.target.value;
-    if (farePortion === "") {
-      farePortion = 0;
+    if(farePortion === "") {
+      farePortion = 0
     }
     totalBase();
     sumAllNumbers();
@@ -93,8 +93,8 @@ if (unusedFare !== null || unusedFare !== undefined) {
 if (surcharge !== null || surcharge !== undefined) {
   surcharge.addEventListener("input", (e) => {
     qSurcharge = e.target.value;
-    if (qSurcharge === "") {
-      qSurcharge = 0;
+    if(qSurcharge ===  "") {
+      qSurcharge = 0
     }
     totalBase();
     sumAllNumbers();
@@ -124,8 +124,8 @@ nucCheckBox.addEventListener("click", (e) => {
 if (penalty !== null || penalty !== undefined) {
   penalty.addEventListener("input", (e) => {
     penalties = e.target.value;
-    if (penalties === "") {
-      penalties = 0;
+    if(penalties === "") {
+      penalties = 0
     }
     sumAllNumbers();
   });
@@ -182,18 +182,13 @@ function sumAllNumbers() {
       parseFloat(totalBaseFare) * parseFloat(nucValue) +
       parseFloat(sumAllTaxes) -
       parseFloat(penalties);
-    totalBaseOutput.innerText = `: ${(
-      parseFloat(totalBaseFare) * parseFloat(nucValue) -
-      parseFloat(penalties)
-    ).toFixed(2)}`;
+      totalBaseOutput.innerText = `: ${(parseFloat(totalBaseFare) * parseFloat(nucValue) - parseFloat(penalties)).toFixed(2)}`
   } else {
     grandTotal =
       parseFloat(totalBaseFare) +
       parseFloat(sumAllTaxes) -
       parseFloat(penalties);
-    totalBaseOutput.innerText = `: ${(
-      parseFloat(totalBaseFare) - parseFloat(penalties)
-    ).toFixed(2)}`;
+      totalBaseOutput.innerText = `: ${(parseFloat(totalBaseFare) - parseFloat(penalties)).toFixed(2)}`
   }
   totalAmount.innerText = `: ${grandTotal.toFixed(2)}`;
 }
